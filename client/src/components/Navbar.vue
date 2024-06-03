84be81a7846e16b68533084d280336e0b404391d
onyillto <56700691+onyillto@users.noreply.github.com>
2024-06-03 10:09:26 +0100

    push

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -54,7 +54,7 @@
                 Blogs
               </router-link>
             </li>
-            <!-- <li>
+             <li>
               <router-link
                 to="/signup"
                 class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
@@ -63,7 +63,7 @@
               >
                 Sign up
               </router-link>
-            </li> -->
+            </li> 
           </ul>
           <div class="lg:hidden">
             <button
