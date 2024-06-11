133a0d0237aa43f309ac59a651399c0f1bddd474
onyillto <56700691+onyillto@users.noreply.github.com>
2024-06-11 12:21:27 +0100

    Update Header.vue

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -11,7 +11,14 @@
   <section class="bg-gray-50 dark:bg-gray-900 bg-cover bg-center" style="background-image: url('https://www.covenantuniversity.edu.ng/images/2020/11/09/covenant-centre-for-learning-resources.jpg'); height: 87vh;">
     <div class="p-8 md:p-12 lg:px-16 lg:py-24 flex items-center justify-center h-full">
      <div class="text-center max-w-lg mx-auto sm:block text-opacity-25">
-  
+  <h2 class="text-2xl font-bold text-stone-900 md:text-3xl dark:text-stone-900 bg-opacity-70 bg-slate-200 font-semibold p-4 rounded">
+    Welcome to the Student Portal
+  </h2>
+
+  <p class="mt-4 text-stone-900 text-lg bg-opacity-70 bg-slate-200 font-semibold p-4 rounded">
+    Welcome to the Student Portal! Connect with job opportunities, manage your work-study program, update your profile, and stay informed with the latest announcements. We're here to help you succeed.
+  </p>
+
 
  
 
