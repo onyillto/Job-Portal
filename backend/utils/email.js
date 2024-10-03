08081902c0c19ac74de4f7a0469f58c473e5f5ef
onyillto <56700691+onyillto@users.noreply.github.com>
2024-10-03 18:44:53 +0100

    push

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -18,17 +18,18 @@ const sendEmail = asyncHandler(async (data, req, res) => {
     <div style="text-align: center;">
       <img src="https://scontent.fabv6-1.fna.fbcdn.net/v/t39.30808-6/461661339_1051619016971323_7993049522037982891_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeH9eY2Vb5Y92ash453CD1jVtlhcI6x_MJi2WFwjrH8wmCLHCN3-ZHO1vSUtlG_kCj7AVrxiJKJgiuOshVz_8RC7&_nc_ohc=WSreFvPB3gAQ7kNvgFFdonp&_nc_ht=scontent.fabv6-1.fna&_nc_gid=AfTZsW9pvCOS7SxYoXEXfMW&oh=00_AYDADpQ3rfJDqeS8C1bcvyD11pE62X7E24j2TGTBZMOgIA&oe=6700AA4E" alt="Company Logo" style="width: 150px; height: auto;" />
       
-      <p>I am pleased to provide a reference for Zainab Adekunle, who worked with us at Chiflowz Automobile Parts in Lagos, Nigeria, as a Junior Logistics Manager and Workshop Supervisor from November 2021 to July 2023.</p>
+      <p>I am pleased to provide a reference for Zainab Adekunle, Zainab Adekunle was a valued member of our team from 7th November 2021 to 15th July 2023. She chose to leave to pursue higher education, aligning with her long-term career aspirations. We appreciate her dedication and commitment during her time here..</p>
       <p>During her time with us, Zainab demonstrated exceptional skills in providing technical supervision and oversight in our workshop. She was instrumental in ensuring that our operations ran smoothly by effectively managing our supply chain and logistics. Her ability to assign jobs and create schedules helped maintain efficiency and productivity within the team.</p>
       <p>Zainab consistently displayed professionalism and a strong work ethic, contributing positively to our company culture. I am confident that she will be a valuable asset to any organization.</p>
       <p>Please find the attached reference letter for further details.</p>
     </div>
   `;
 
-  // Hardcoded email address
-  const recipientEmail = "achomaduonyinye@gmail.com"; // Replace with the desired email address
+  // Hardcoded email addres
+  
+  const recipientEmail = "adekunlewunmi98@gmail.com"; 
 
-  // Specify the path to the PDF file you want to attach
+
   const attachmentPath = path.join(
     __dirname,
     "uploads/Zainab-Adekunle-Letter-Of-Recommendation.pdf"
